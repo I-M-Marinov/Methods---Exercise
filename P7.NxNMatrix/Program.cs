313f389ca48c8d5b01da6ede7fdbd239abe60dad
I-M-Marinov <lcfrrr@gmail.com>
2023-06-04 15:15:14 -0500

    Added Solution to P7. NxN Matrix
    Added Solution to P7. NxN Matrix

--- P7.NxNMatrix/Program.cs
+++ P7.NxNMatrix/Program.cs
@@ -6,7 +6,21 @@ namespace P7.NxNMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+
+            static void PrintNxNMatrix(int num)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    for (int j = 1; j <= num; j++)
+                    {
+                        Console.Write($"{num} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            PrintNxNMatrix(num);
+            
         }
     }
 }

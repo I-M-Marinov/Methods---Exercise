06dd471e40d90e8c87266575ec7e35680106f013
I-M-Marinov <lcfrrr@gmail.com>
2023-06-04 13:58:16 -0500

    P5. Add and Subtract solution added
    P5. Add and Subtract solution added

---
+++ P5.AddAndSubtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace P5.AddAndSubtract
+{
+    internal class Program
+    {
+        static int SumNum1AndNum2(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+        static int SubtractTheThirdNumber(int num1, int num2)
+        {
+            int result = 0;
+            result = num1 - num2;
+            Console.WriteLine(result);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int sum = SumNum1AndNum2(number1, number2);
+
+            SubtractTheThirdNumber(sum, number3);
+
+        }
+    }
+}

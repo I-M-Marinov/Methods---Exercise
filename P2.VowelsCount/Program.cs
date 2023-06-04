8a1a919986a2d2709998938f3d9a4d6b01bba51c
I-M-Marinov <lcfrrr@gmail.com>
2023-06-04 05:33:10 -0500

    Added P2 and P3 solutions
    Added P2 and P3 solutions

--- P2.VowelsCount/Program.cs
+++ P2.VowelsCount/Program.cs
@@ -4,9 +4,34 @@ namespace P2.VowelsCount
 {
     internal class Program
     {
+        static int GetNumberOfVowels(string a, char[] b)
+        {
+            int vowelsCount = 0;
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (b[i] == 'a' || b[i] == 'e' || b[i] == 'i' ||
+                    b[i] == 'o' || b[i] == 'u' || b[i] == 'y' || 
+                    b[i] == 'A' || b[i] == 'E' || b[i] == 'I' || 
+                    b[i] == 'O' || b[i] == 'U' || b[i] == 'Y')
+                {
+                    vowelsCount += 1;
+                }
+            }
+
+            Console.WriteLine(vowelsCount);
+            return vowelsCount;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //*  A, E, I, O, U, Y  *//
+
+            string inputText = Console.ReadLine();
+            char[] stringToArray = inputText.ToCharArray();
+
+            GetNumberOfVowels(inputText, stringToArray);
+
+
+
         }
     }
 }
---
+++ P3.CharactersInRange/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace P3.CharactersInRange
+{
+    internal class Program
+    {
+        
+        static int ConvertIntToCharAndPrint(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                for (int i = num1 + 1; i < num2; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+            else if (num1 > num2)
+            {
+                for (int i = num2 + 1; i < num1; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+
+            return num1;
+        }
+        static void Main(string[] args)
+        {
+            
+
+            char character1 = char.Parse(Console.ReadLine());
+            char character2 = char.Parse(Console.ReadLine());
+            int num1 = (int)character1;
+            int num2 = (int)character2;
+
+            ConvertIntToCharAndPrint(num1, num2);
+
+        }
+    }
+}

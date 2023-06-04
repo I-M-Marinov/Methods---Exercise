0c94f3a6509241b62cae6308769e7ac4a529435a
I-M-Marinov <lcfrrr@gmail.com>
2023-06-04 14:46:52 -0500

    P6 and P7 Solutions
    Added P6 and P7 Solutions

---
+++ P6.MiddleCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace P6.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] stringToArray = input.ToCharArray();
+
+            static string FindTheMiddleCharacter(string input)
+            {
+                int extraDigit = 1 - input.Length % 2; 
+                return input.Substring(input.Length / 2 - extraDigit, 1 + extraDigit);
+
+            }
+            Console.WriteLine(FindTheMiddleCharacter(input));
+            FindTheMiddleCharacter(input);
+        }
+    }
+}
---
+++ P7.NxNMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P7.NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

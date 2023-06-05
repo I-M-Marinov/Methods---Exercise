64572d5422addde8fb60e0861e1137933b2544fc
I-M-Marinov <lcfrrr@gmail.com>
2023-06-05 09:39:58 -0500

    Added solution to P8. Factorial Division
    Added solution to P8. Factorial Division

---
+++ P8.FactorialDivision/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P8.FactorialDivision
+{
+    internal class Program
+    {
+        static int CalculateFactorialOfANumber(int number)
+        {
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+               fact = fact * i;
+            }
+            return fact;
+        }
+
+        static void DivideTheTwoFactorials(int number, int number2)
+        {
+            int result = number / number2;
+            Console.WriteLine($"{result:f2}");
+        }
+   
+
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            int factorial1 = CalculateFactorialOfANumber(number1);
+            int factorial2 = CalculateFactorialOfANumber(number2);
+
+            DivideTheTwoFactorials(factorial1,factorial2);
+
+        }
+    }
+}
